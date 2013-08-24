ede58cb94bfdc61f8da704072da502b0f2ca9047
Richard Pijnenburg <richard@ispavailability.com>
2013-08-24 02:30:39 +0200

    Change spec helper

--- spec/spec_helper_system.rb
+++ spec/spec_helper_system.rb
@@ -4,25 +4,22 @@ require 'rspec-system-puppet/helpers'
 include RSpecSystemPuppet::Helpers
 
 RSpec.configure do |c|
-  # Project root for the firewall code
+  # Project root
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
-  # Enable colour in Jenkins
+  # Enable colour
   c.tty = true
 
-  # This is where we 'setup' the nodes before running our tests
-  c.system_setup_block = proc do
-    # TODO: find a better way of importing this into this namespace
-    c.include RSpecSystemPuppet::Helpers
+  c.include RSpecSystemPuppet::Helpers
 
+  # This is where we 'setup' the nodes before running our tests
+  c.before :suite do
     # Install puppet
     puppet_install
     puppet_master_install
 
     system_run('puppet module install puppetlabs/stdlib')
-
     # Replace mymodule with your module name
     puppet_module_install(:source => proj_root, :module_name => 'elasticsearch')
   end
 end
-
